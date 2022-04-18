002ce6d0c8da8b9695ed9fd7569bfb450ad8cfac
Stimils02 <99268212+Stimils02@users.noreply.github.com>
2022-04-18 13:27:45 -0400

    Add files via upload

---
+++ Contest.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Contest {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        int k = sc.nextInt();
+        ArrayList<Integer> ranks = new ArrayList<>(p);
+        for (int i = 0; i < p; i++) {
+            ranks.add(sc.nextInt());
+        }
+        Collections.sort(ranks);
+
+        int v = 0;
+        int teams = 0;
+        while (teams < k) {
+            teams = 0;
+            for(int i = 0; i < p - 2; i++) {
+                if (ranks.get(i+2) - ranks.get(i) <= v) {
+                    teams++;
+                    i += 2;
+                }
+            }
+            v++;
+        }
+        System.out.println(v-1);
+    }
+}
