1bf0d44750a28f587d1932747e8bc8eb22992451
Stimils02 <99268212+Stimils02@users.noreply.github.com>
2022-04-18 13:35:15 -0400

    Add files via upload

--- Contest.java
+++ Contest.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Contest {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
         Scanner sc = new Scanner(System.in);
         int p = sc.nextInt();
         int k = sc.nextInt();
